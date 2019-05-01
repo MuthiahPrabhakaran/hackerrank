35b55c90e67100f0b4d7b7437b22bd8faae7198f
Muthiah Prabhakaran <muthiahprabhakaran@gmail.com>
2019-05-01 12:23:48 +0530

    Counting valleys - completed

--- InterviwPreparationKit/src/main/java/com/hackerrank/warmup/CountingValleys.java
+++ InterviwPreparationKit/src/main/java/com/hackerrank/warmup/CountingValleys.java
@@ -7,55 +7,24 @@ public class CountingValleys {
 
 	public static void main(String[] args) {
 
-		int n = scanner.nextInt();
-		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
 		String s = scanner.nextLine();
-
-		int result = countingValleys(n, s);
+		int result = countingValleys(s);
 		System.out.println(result);
 		scanner.close();
-
 	}
-
-	static int countingValleys(int n, String steps) {
-
-		int count = 0;
-		int initialSeaLevelStepsCount = 0;
-		int seaLevelCount = 0;
-
-		if (steps.startsWith("U")) {
-			initialSeaLevelStepsCount = ignoreTillSeaLevel(steps);
-		}
-
-		for (int i = initialSeaLevelStepsCount; i < steps.length(); i++) {
-			if (new Character(steps.charAt(i)).compareTo('U') == 0) {
-				seaLevelCount = seaLevelCount + 1;
-			} else {
-				seaLevelCount = seaLevelCount - 1;
-			}
-			if (seaLevelCount == 0) {
-				count++;
-			}
-		}
-		return count;
-
-	}
-
-	static int ignoreTillSeaLevel(String stepsTrack) {
-		int seaLevelDistance = 1;
-		int steps = 1;
-		int valley = -1;
-		for (int i = 1; i < stepsTrack.length(); i++, steps++) {
-			if (seaLevelDistance == 0)
-				break;
-			if (new Character(stepsTrack.charAt(i)).compareTo('U') == 0) {
-				seaLevelDistance += 1;
-			} else {
-				seaLevelDistance = seaLevelDistance + valley;
-			}
-		}
-		return steps;
+	static int countingValleys(String steps) {
+		int v = 0;     // # of valleys
+        int lvl = 0;   // current level
+        for(char c : steps.toCharArray()){
+            if(c == 'U') ++lvl;
+            if(c == 'D') --lvl;
+            // if we just came UP to sea level
+            if(lvl == 0 && c == 'U')
+                ++v;
+        }
+        return v;
 	}
 
 }
+
+
